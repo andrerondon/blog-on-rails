ad029b787e3504a14472a3c97d61f24fe5314f33
andre-rondon <rondon@estudiorondon.com.br>
2020-05-07 22:40:01 -0700

    seed and faker running for posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Post.delete_all 
+
+NUM_POSTS = 50
+
+NUM_POSTS.times do 
+    created_at = Faker::Date.backward(days: 365 * 5)
+        Post.create(
+            title:Faker::Nation.capital_city,
+            body: Faker::Lorem.sentences,
+            created_at: created_at,
+            updated_at: created_at
+    )
+  
+end
+
+
+post = Post.all 
+
+
+puts Cowsay.say("Generated #{post.count} projects", :frogs)
+# puts Cowsay.say("Generated #{task.count} tasks", :tux)
