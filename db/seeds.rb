432aeeec14d2d2553d0a9417890f59d9592cbdce
andre-rondon <rondon@estudiorondon.com.br>
2020-05-08 09:29:07 -0700

    posts show page well done

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,12 +19,10 @@ NUM_POSTS.times do
             created_at: created_at,
             updated_at: created_at
     )
-  
 end
 
 
 post = Post.all 
-
 
 puts Cowsay.say("Generated #{post.count} projects", :frogs)
 # puts Cowsay.say("Generated #{task.count} tasks", :tux)
